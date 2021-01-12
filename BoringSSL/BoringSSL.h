da0afe4235aa0e70969fbc11e2f75ea2ee9fbca6
mafeng.lgs <mafeng.lgs@taobao.com>
2021-01-12 18:03:23 +0800

    project update

---
+++ BoringSSL/BoringSSL.h
@@ -0,0 +1,19 @@
+//
+//  BoringSSL.h
+//  BoringSSL
+//
+//  Created by enos sariel on 2020/4/6.
+//  Copyright © 2020 enos sariel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BoringSSL.
+//FOUNDATION_EXPORT double BoringSSL_VersionNumber;
+
+//! Project version string for BoringSSL.
+//FOUNDATION_EXPORT const unsigned char BoringSSL_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BoringSSL/PublicHeader.h>
+
+
